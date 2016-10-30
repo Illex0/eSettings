e9fda919635263e2c41209646840e0342b325b0c
pcuco <pedrocuco25@gmail.com>
2016-10-30 22:36:23 +0000

    **** SIMCard *** getNumber function update

--- app/src/main/java/com/app/esettings/MainActivity.java
+++ app/src/main/java/com/app/esettings/MainActivity.java
@@ -9,7 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // SIM Card number test
         String sim = SIMCard.getNumber(getApplicationContext());
+
+
     }
 
 
--- app/src/main/java/com/app/esettings/SIMCard.java
+++ app/src/main/java/com/app/esettings/SIMCard.java
@@ -11,8 +11,15 @@ public class SIMCard {
 
         // GET SIM Card NUmber
         public static String getNumber(Context mContext){
+            String simNumber;
             TelephonyManager mTelephonyManager = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-            String simNumber = mTelephonyManager.getLine1Number();
+
+            // @TODO - Aadroid 6 permissions
+            // @TODO - Dual SIM
+            if (mTelephonyManager.getLine1Number() != null) {
+                simNumber = mTelephonyManager.getLine1Number();
+            } else
+                simNumber = "NO CArd";
 
             if (simNumber != null)
                 Toast.makeText(mContext,simNumber, Toast.LENGTH_SHORT).show();
