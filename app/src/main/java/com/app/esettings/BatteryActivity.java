704b993cfd8fecd1d0796390d4d88ea807b6a6d3
Luís Ferreira <luisferreira.inf@gmail.com>
2016-11-22 14:56:55 +0000

    Battery image changes according to battery percentage in the layout

--- app/src/main/java/com/app/esettings/BatteryActivity.java
+++ app/src/main/java/com/app/esettings/BatteryActivity.java
@@ -2,6 +2,8 @@ package com.app.esettings;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,6 +23,23 @@ public class BatteryActivity extends AppCompatActivity {
         TextView voltage = (TextView) findViewById(R.id.voltage);
         TextView plugged = (TextView) findViewById(R.id.plugged);
         TextView percentage = (TextView) findViewById(R.id.percentage);
+        ImageView BatteryLow = (ImageView) findViewById(R.id.imageView2);
+        ImageView BatteryMediumLow = (ImageView) findViewById(R.id.imageView3);
+        ImageView BatteryMediumHigh = (ImageView) findViewById(R.id.imageView4);
+        ImageView BatteryHigh = (ImageView) findViewById(R.id.imageView5);
+        String percentage2 = Battery.batteryPercentage(this.getApplicationContext());
+
+        int per = Integer.parseInt(percentage2);
+
+        if (per <= 25) {
+            BatteryLow.setVisibility(View.VISIBLE);
+        } else if (per > 25 && per <= 50) {
+            BatteryMediumLow.setVisibility(View.VISIBLE);
+        } else if (per > 50 && per <= 75) {
+            BatteryMediumHigh.setVisibility(View.VISIBLE);
+        } else {
+            BatteryHigh.setVisibility(View.VISIBLE);
+        }
 
         temperature.setText(Battery.batteryTemperature(this.getApplicationContext()));
         voltage.setText(Battery.batteryVoltage(this.getApplicationContext()));
